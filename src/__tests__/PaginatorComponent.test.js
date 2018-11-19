27a75202b420bf044bbd0de3b959167cd9443c10
Lucas Aquiles <lucasaquiles@gmail.com>
2018-11-19 02:57:52 -0200

    configure mock store

--- src/__tests__/PaginatorComponent.test.js
+++ src/__tests__/PaginatorComponent.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { shallow, mount, render } from  'enzyme';
+import configureStore from 'redux-mock-store';
+import PaginatorComponent from '../PaginatorComponent';
 
-import PaginatorComponent from '../PaginatorComponent'
+const mockStore = configureStore();
+const store = mockStore({});
 
 describe('Paginator', () => {
 
     it('should render ', ()=> {
-        const component = shallow (<PaginatorComponent debug />)
-        expect(component).toMatchSnapshot();
+        const wrapper = shallow (<PaginatorComponent  />, {context: {store}})
+        const component = wrapper.dive()
+
+        console.log(wrapper);        
     });
 })
